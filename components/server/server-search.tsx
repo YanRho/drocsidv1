36da477977dd06f79c313f2db77f382971bcc158
Bryan Bergenholtz <116790009+YanRho@users.noreply.github.com>
2024-11-01 17:32:03 -0700

    Server Channel List Done!

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -33,7 +33,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+      if (e.key.toLowerCase() === "k" && (e.metaKey || e.ctrlKey)) {
         console.log("Ctrl + K pressed");
         e.preventDefault();
         setOpen((open) => !open);
